56e8f42eed3f8d0627385947cc0e83525f53eb3e
bandiEswar <121429247+bandiEswar@users.noreply.github.com>
2023-07-19 15:44:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function OFF(){
+    document.getElementById("bulb").src="http://www.chineselight.com/uploads/130107/1_114009_1.jpg";
+}
+function ON(){
+    document.getElementById("bulb").src="https://www.christmaslightsetc.com/MediaService/25048";
+}
